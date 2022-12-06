77b11cd18dddd3f59b7c2d50d3d6ff292139c945
GeserAllan <geserallan@gmail.com>
2022-12-05 21:29:29 -0300

    Update script.js

--- exercícios/exercícios-progbr/Java Script/Operadores Aritméticos/script.js
+++ exercícios/exercícios-progbr/Java Script/Operadores Aritméticos/script.js
@@ -6,8 +6,14 @@
 // ++ Incremento
 // -- Decremento
 
-var a = 10;
-var b = 15;
-var c= a + b;
+// var a = 10;
+// var b = 15;
+// var c= a + b;
 
-console.log(c);
+// console.log(c);
+
+
+var a = 10;
+var b = a--;
+
+console.log(b)
