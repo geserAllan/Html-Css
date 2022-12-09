93aff227f85f1fc5e2a0223db8ea5f49f6ccbc52
GeserAllan <geserallan@gmail.com>
2022-12-09 15:48:42 -0300

    fgh

---
+++ exercícios/exercícios-progbr/Java Script/Operador Comparativo/script.js
@@ -0,0 +1,11 @@
+// ==  igual a
+// === valor igual e igual 
+// !=  não e igual
+// !== não igual ou não igual
+// >   maior que
+// <   menor que
+// >=  maior que ou igual a
+// <=  menor que ou igual a
+
+
+console.log( 2 == 2);
