5781318c72dd8db63b634ed7143d4aa3359e3019
GeserAllan <geserallan@gmail.com>
2022-12-09 16:29:14 -0300

    nb

--- exercícios/exercícios-progbr/Java Script/Operadores Lógicos/script.js
+++ exercícios/exercícios-progbr/Java Script/Operadores Lógicos/script.js
@@ -2,9 +2,22 @@
 // || ou
 //    negativo
 
-var a = false;
-var b = false;
+// var a = false;
+// var b = false;
 
-var c = a && b;
+// var c = a && b;
 
-console.log(c);
+// console.log(c);
+
+
+var idade = 20;
+
+var maior20 = idade >= 20;
+var menor30 = idade <= 30;
+
+var entre = maior20 && menor30;
+
+console.log('idade', Idade);
+console.log('maior que 20', maior20);
+console.log('menor que 30', menor30);
+console.log('entre 20 e 30', entre);
