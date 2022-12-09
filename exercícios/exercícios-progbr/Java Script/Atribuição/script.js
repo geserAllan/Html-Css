926c9c00cadd0562128a71bd5eb07027d3285bcf
GeserAllan <geserallan@gmail.com>
2022-12-09 15:20:31 -0300

    gfdffv

---
+++ exercícios/exercícios-progbr/Java Script/Atribuição/script.js
@@ -0,0 +1,13 @@
+// =    x = y   x = y
+// +=   x += y  x = x + y
+// -=   x -= y  x = x - y
+// *=   x *= y  x = x * y
+// /=   x /= y  x = x / y
+// %=   x %= y  x = x % y
+
+
+var a = 30;
+
+a += 2;
+
+console.log(a)
