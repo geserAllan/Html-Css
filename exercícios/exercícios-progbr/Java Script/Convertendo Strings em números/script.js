f985165f27d658b52ae0e7ce87c0738e169aa2aa
GeserAllan <geserallan@gmail.com>
2022-12-11 20:13:50 -0300

    fg

---
+++ exercícios/exercícios-progbr/Java Script/Convertendo Strings em números/script.js
@@ -0,0 +1,13 @@
+
+// var a = "3";
+// var b = "6";
+// var c =  parseInt(a) + parseInt(b);
+
+// console.log(c);
+
+
+var a = "3.5";
+var b = "4";
+var c = parseFloat(a) + parseInt(b);
+
+console.log(c);
