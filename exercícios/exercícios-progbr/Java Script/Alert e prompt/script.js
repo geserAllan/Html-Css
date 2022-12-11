f18c7812ff4cdfd5e0c858138b7c92cf63d81cc5
GeserAllan <geserallan@gmail.com>
2022-12-11 20:56:15 -0300

    hj

---
+++ exercícios/exercícios-progbr/Java Script/Alert e prompt/script.js
@@ -0,0 +1,12 @@
+// alert("Qual o seu nome?")
+
+// var nome = prompt("Qual o seu nome?")
+
+// alert("Olá " + nome)
+
+
+var número = parseInt(prompt("Digite um número:"))
+
+var dobro = número + número
+
+alert("O dobro de " + número + " é " + dobro)
