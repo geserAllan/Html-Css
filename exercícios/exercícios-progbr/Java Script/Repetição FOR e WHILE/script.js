b3f7b0ddc0bda7445cd29435b07873d99cc4bbe0
GeserAllan <geserallan@gmail.com>
2022-12-17 17:06:48 -0300

    gh

---
+++ exercícios/exercícios-progbr/Java Script/Repetição FOR e WHILE/script.js
@@ -0,0 +1,18 @@
+// var número = 5;
+
+// for (var i = 0; i < 5; i++){
+
+//     console.log("Rodando laço " + i + "vez")
+
+// }
+// console.log("Acabou")
+
+
+var numero = 5;
+
+while(numero < 10){
+
+    console.log("Número " + numero);
+    numero++;
+}
+console.log("Acabou")
