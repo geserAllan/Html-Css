1cfd0dab47ad2a31a89734bac68ef980a7a7620b
GeserAllan <geserallan@gmail.com>
2022-12-17 11:11:52 -0300

    operador ternario

---
+++ exercícios/exercícios-progbr/Java Script/Operador ternário/script.js
@@ -0,0 +1,9 @@
+// var idade = 16
+
+// var permissão = idade >=18 ? true : false;
+
+// console.log(permissão);
+
+var age = 18;
+
+var permission = age >= 18 ? console.log ("Permitido") : console.log ("Negado")
