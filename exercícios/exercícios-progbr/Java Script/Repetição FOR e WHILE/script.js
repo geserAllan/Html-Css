0f9617edf0d433c9a928af967f18d215c8d0e1df
GeserAllan <geserallan@gmail.com>
2022-12-17 21:21:33 -0300

    forwhile

--- exercícios/exercícios-progbr/Java Script/Repetição FOR e WHILE/script.js
+++ exercícios/exercícios-progbr/Java Script/Repetição FOR e WHILE/script.js
@@ -8,11 +8,28 @@
 // console.log("Acabou")
 
 
-var numero = 5;
 
-while(numero < 10){
 
-    console.log("Número " + numero);
-    numero++;
+// var numero = 5;
+
+// while(numero < 10){
+
+//     console.log("Número " + numero);
+//     numero++;
+// }
+// console.log("Acabou")
+
+
+
+var numero = Math.random() * 100;
+
+while (numero < 90){
+   
+    console.log(numero);
+    numero = Math.random() * 100;
+
+
 }
-console.log("Acabou")
+
+console.log(numero);
+console.log("Acabou");
