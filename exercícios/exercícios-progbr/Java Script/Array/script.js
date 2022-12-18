31c8fa170c26bdc5fca7692f9fe4646d7b0037b2
GeserAllan <geserallan@gmail.com>
2022-12-18 18:46:43 -0300

    jk

---
+++ exercícios/exercícios-progbr/Java Script/Array/script.js
@@ -0,0 +1,43 @@
+
+
+// var nomes = new Array("Alan","Bruna","Gustavo","Maria","Carla");
+
+// console.log(nomes[2]);
+
+
+
+
+// var nomes = ["Alan","Bruna","Gustavo","Maria","Carla"];
+
+// console.log(nomes.length);
+
+
+
+
+
+// var nomes = ["Alan","Bruna","Gustavo","Maria","Carla"];
+
+// for (var i = 0; i < nomes.length; i++){
+//     console.log(nomes[i])
+// }
+
+
+
+
+
+// var nomes = ["Alan","Bruna","Gustavo","Maria","Carla"];
+// for (var i in nomes){
+//     console.log(nomes[i]);
+
+// }
+
+
+
+
+
+var nomes = ["Alan","Bruna","Gustavo","Maria","Carla"];
+for (var nome of nomes){
+    console.log(nome);
+
+}
+
