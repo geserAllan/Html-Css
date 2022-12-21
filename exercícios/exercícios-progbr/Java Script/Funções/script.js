b7cb6837bbd23e2ed478a1b37e3a166a33149407
GeserAllan <geserallan@gmail.com>
2022-12-20 21:19:46 -0300

    j

--- exercícios/exercícios-progbr/Java Script/Funções/script.js
+++ exercícios/exercícios-progbr/Java Script/Funções/script.js
@@ -12,5 +12,12 @@
 // average();
 
 
-function average();
-    var
+function average(s1, s2){
+    var score1 = s1;
+    var score2 = s2;
+    var average = (score1 + score2) / 2;
+    console.log(average);
+}
+
+average(7, 9);
+average(3, 7.8);
