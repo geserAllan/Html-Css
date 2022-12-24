69df1d6f1fb7bde331446cb6e34242cc1c001f3b
GeserAllan <geserallan@gmail.com>
2022-12-24 10:23:16 -0300

    bn

---
+++ exercícios/exercícios-progbr/Java Script/Objetos - Propriedades/script.js
@@ -0,0 +1,79 @@
+// var aluno = {
+//             nome:"Alan", 
+//             idade: 34, 
+//             nota: 8.9
+// }
+
+// console.log(aluno.nome);
+
+
+
+
+
+
+
+// var aluno = {
+//     nome:"Alan", 
+//     idade: 34,      
+//     nota: 8.9
+// }
+
+// console.log(aluno["nome"]);  /*para acessar uma propriedade dentro de colchetes, usar os aspas.*/
+
+
+
+
+// var aluno = {
+//     nome:"Alan", 
+//     idade: 34,      
+//     notas: [8.9, 9.0]
+// }
+
+// console.log(aluno.notas[1]);
+
+
+
+
+
+
+
+// var aluno = {
+//     nome:"Alan", 
+//     idade: 34,      
+//     notas: [8.9, 9.0]
+// }
+
+// console.log(aluno["notas"][1]);
+
+
+
+
+
+/*adicionando propriedades*/
+
+var aluno = {
+    nome:"Alan", 
+    idade: 34,      
+    notas: [8.9, 9.0]
+}
+
+aluno.matrícula = 246810; /*inserção de nova propriedade*/
+
+aluno ["sobrenome"] = "Oliveira";  /*outra forma de inserção de propriedade*/
+
+
+console.log(aluno);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
