1ba8f5e1e17df54be9a8bcb806888cca61503eae
GeserAllan <geserallan@gmail.com>
2022-12-25 11:45:08 -0300

    nm

--- exercícios/exercícios-progbr/Java Script/Objetos -  Construtores/script.js
+++ exercícios/exercícios-progbr/Java Script/Objetos -  Construtores/script.js
@@ -21,17 +21,56 @@
 
 
 
-function criarAluno(nome, n1, n2){
-        return  {
-                nome: nome,
-                nota1: n1,
-                nota2: n2,
 
-                media: function(){
-                    return(this.nota1 + this.nota2) / 2;
-                }
-        }
+
+
+
+
+
+// function criarAluno(nome, n1, n2){
+//         return  {
+//                 nome: nome,
+//                 nota1: n1,
+//                 nota2: n2,
+
+//                 media: function(){
+//                     return(this.nota1 + this.nota2) / 2;
+//                 }
+//         }
         
+// }
+
+
+// var turma = [
+//             criarAluno("João", 9.0, 8.0),
+//             criarAluno("Carla", 5.0, 8.0),
+//             criarAluno("Marcos", 8, 5)
+// ]
+
+// var aluno = turma[1];
+// console.log(aluno);
+// console.log(aluno.media());
+
+
+
+
+
+
+
+
+
+
+function criarAluno(nome, n1, n2){
+    return  {
+            nome: nome,
+            nota1: n1,
+            nota2: n2,
+
+            media: function(){
+                return(this.nota1 + this.nota2) / 2;
+            }
+    }
+    
 }
 
 
@@ -39,8 +78,12 @@ var turma = [
             criarAluno("João", 9.0, 8.0),
             criarAluno("Carla", 5.0, 8.0),
             criarAluno("Marcos", 8, 5)
-]
+    ]
 
 var aluno = turma[1];
-console.log(aluno);
-console.log(aluno.media());
+
+turma.forEach(function (elemento){
+
+            console.log(elemento);
+
+})
