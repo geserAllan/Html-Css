89a3b90538d1b77e2bd534e7ae8018a4cb9fd991
GeserAllan <geserallan@gmail.com>
2022-12-25 20:07:52 -0300

    Update script.js

--- exercícios/exercícios-progbr/Java Script/Objetos -  Construtores/script.js
+++ exercícios/exercícios-progbr/Java Script/Objetos -  Construtores/script.js
@@ -60,30 +60,63 @@
 
 
 
-function criarAluno(nome, n1, n2){
-    return  {
-            nome: nome,
-            nota1: n1,
-            nota2: n2,
 
-            media: function(){
-                return(this.nota1 + this.nota2) / 2;
-            }
-    }
+
+// // function criarAluno(nome, n1, n2){
+// //     return  {
+// //             nome: nome,
+// //             nota1: n1,
+// //             nota2: n2,
+
+// //             media: function(){
+// //                 return(this.nota1 + this.nota2) / 2;
+// //             }
+// //     }
     
+// // }
+
+
+// // var turma = [
+// //             criarAluno("João", 9.0, 8.0),
+// //             criarAluno("Carla", 5.0, 8.0),
+// //             criarAluno("Marcos", 8, 5)
+// //     ]
+
+// // var aluno = turma[1];
+
+// // for(var aluno of turma){
+// //     console.log(aluno.nome + " - " + aluno.media());
+   
+// }
+
+
+
+
+
+
+
+
+
+
+function aluno(nome, n1, n2){
+    this.nome = nome;
+    this.nota1 = n1;
+    this.nota2 = n2;
+
+    this.media = function(){
+        return( this.nota1 + this.nota2) / 2;
+    }
 }
 
+    var a = new aluno("Alan", 6, 8);
+    var b = new aluno("Vanessa", 5 , 8);
 
-var turma = [
-            criarAluno("João", 9.0, 8.0),
-            criarAluno("Carla", 5.0, 8.0),
-            criarAluno("Marcos", 8, 5)
-    ]
+    console.log(a.nome + " - " + a.media());
+    console.log(b.nome + " - " + b.media());
 
-var aluno = turma[1];
 
-turma.forEach(function (elemento){
 
-            console.log(elemento);
 
-})
+
+
+
