cf7edafcb50a388f793bab579fe7e5a217c703ff
GeserAllan <geserallan@gmail.com>
2022-12-26 17:15:14 -0300

    jk

---
+++ exercícios/exercícios-progbr/Java Script/Const, Let e var/script.js
@@ -0,0 +1,26 @@
+// var a = 5;
+// let b = 7;
+// const c = {};
+
+// c.nome = "Alan"
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+
+
+var a = 5;
+let b = 7;
+const c = [4];
+
+c.push(3);
+
+
+console.log(a);
+console.log(b);
+console.log(c);
