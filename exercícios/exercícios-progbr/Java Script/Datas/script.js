d91fef62b9c834e6bcbeaaa41c3905358101b52d
GeserAllan <geserallan@gmail.com>
2022-12-26 12:44:08 -0300

    nm

---
+++ exercícios/exercícios-progbr/Java Script/Datas/script.js
@@ -0,0 +1,59 @@
+// var d = new Date();
+
+// console.log(d);
+
+
+
+
+
+
+
+// var d = new Date(year, month, day, hours, minutes, seconds ,miliseconds);
+
+// console.log(d);
+
+
+
+
+
+
+
+// var d = new Date(2018, 08, 12);
+
+// console.log(d.getFullYear());
+
+
+
+
+
+
+// var d = new Date();
+
+// var dias = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"]
+
+// console.log(d);
+// console.log(dias[d.getDay()]);
+
+
+
+
+
+
+// var d = new Date("Dec 22 2022 23:25");
+
+// console.log(d);
+
+
+
+
+
+
+
+
+
+var d = new Date("Dec 22 2022 23:25");
+
+console.log(d);
+console.log(d.setDate(23));
+console.log(d);
+
