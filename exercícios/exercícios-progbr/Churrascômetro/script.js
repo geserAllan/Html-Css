02c963ceffd3c422fbbc24341db6c589eed08ad8
GeserAllan <geserallan@gmail.com>
2023-03-05 20:11:29 -0300

    Update script.js

---
+++ exercícios/exercícios-progbr/Churrascômetro/script.js
@@ -0,0 +1,7 @@
+// Carne - 450g por pessoa. + de 6 horas de evento = 600g por pessoa.
+// Cerveja - 1200ml por pessoa. + de 6 horas de evento = 2000ml por pessoa.
+// Refrigerante/Água - 1000ml por pessoa. + de 6 horas de evento = 1500ml por pessoa.
+
+//Crianças valem por 0,5
+
+
