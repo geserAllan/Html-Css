422418c38307b3070b4d61473aa3cc9be690149d
GeserAllan <geserallan@gmail.com>
2023-03-07 19:24:44 -0300

    fdh

--- exercícios/exercícios-progbr/Churrascômetro/script.js
+++ exercícios/exercícios-progbr/Churrascômetro/script.js
@@ -1,12 +1,32 @@
-// Carne - 450g por pessoa. + de 6 horas de evento = 600g por pessoa.
+// Carne - 400g por pessoa. + de 6 horas de evento = 650g por pessoa.
 // Cerveja - 1200ml por pessoa. + de 6 horas de evento = 2000ml por pessoa.
 // Refrigerante/Água - 1000ml por pessoa. + de 6 horas de evento = 1500ml por pessoa.
 
 //Crianças valem por 0,5
 
 let inputAdultos = document.getElementById("adultos");
-let inputCrianças = document.getElementById("criancas");
-let inputDuração = document.getElementById("duracao");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
 
-console.log(inputAdultos.value, inputCrianças.value, inputDuração.value );
+let resultado = document.getElementById("resultado");
 
+function calcular(){
+    console.log("Calculando...");
+
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    let qtdTotalCarne = carnePP(duracao) * adultos + ( 400 / 2 * criancas);
+    console.log(qtdTotalCarne);
+
+}
+
+function carnePP(duracao){
+    if (duracao >= 6){
+        return 650;
+    }else {
+        return 400;
+    }
+
+}
