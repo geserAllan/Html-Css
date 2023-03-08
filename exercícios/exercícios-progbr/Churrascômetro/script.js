894760508d5713958650a3b822dfc00dc7ccdf26
GeserAllan <geserallan@gmail.com>
2023-03-07 21:36:16 -0300

    f

--- exercícios/exercícios-progbr/Churrascômetro/script.js
+++ exercícios/exercícios-progbr/Churrascômetro/script.js
@@ -11,15 +11,20 @@ let inputDuracao = document.getElementById("duracao");
 let resultado = document.getElementById("resultado");
 
 function calcular(){
-    console.log("Calculando...");
+console.log("Calculando...");
 
     let adultos = inputAdultos.value;
     let criancas = inputCriancas.value;
     let duracao = inputDuracao.value;
+    
+    let qtdTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
+    let qtdTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
+    let qtdTotalCerveja = cervejaPP(duracao) * adultos;
 
-    let qtdTotalCarne = carnePP(duracao) * adultos + ( 400 / 2 * criancas);
-    console.log(qtdTotalCarne);
-
+    resultado.innerHTML = `<p>${qtdTotalCarne / 1000} Kg de Carne </p>`
+    resultado.innerHTML += `<p>${qtdTotalBebidas / 2000} Lts de Bebidas </p>`
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja / 355)} latas de Cerveja </p>`
+    
 }
 
 function carnePP(duracao){
@@ -27,6 +32,22 @@ function carnePP(duracao){
         return 650;
     }else {
         return 400;
+    }
+
+}
+function cervejaPP(duracao){
+    if (duracao >= 6){
+        return 2000;
+    }else {
+        return 1200;
+    }
+
+}
+function bebidasPP(duracao){
+    if (duracao >= 6){
+        return 1500;
+    }else {
+        return 1000;
     }
 
 }
